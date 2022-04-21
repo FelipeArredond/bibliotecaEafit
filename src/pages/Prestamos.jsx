e27c1257538f6d54f34c6be6f576132441669da4
FelipeArredond <pipechiqui77@gmail.com>
2022-04-21 16:10:13 -0500

    Prestamos Updated

--- src/pages/Prestamos.jsx
+++ src/pages/Prestamos.jsx
@@ -1,7 +1,22 @@
+import { Form, FormGroup, FormLabel, FormText, FormControl ,Button} from "react-bootstrap"
+import { useState, useEffect } from "react"
+
 export default function Prestamos(){
+
     return(
         <div>
             <h3>Prestamos</h3>
+            <Form>
+            <FormGroup className="mb-3" controlId="formBasicEmail">
+                <FormLabel>Ingresa el ID del libro que deseas prestar</FormLabel>
+                <FormControl type="email" placeholder="ID" />
+                </FormGroup>
+                <FormGroup>    
+                <Button variant="primary" type="submit">
+                    Buscar
+                </Button>
+                </FormGroup>    
+            </Form>
         </div>
     )
 }
